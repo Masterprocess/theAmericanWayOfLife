ed8cd73f120fa91fc2efbe41f9e981b6d0bee098
Christopher Rackley <masterprocess@The-Beluga.local>
2017-08-30 19:14:04 -0400

    fixed functionality bug, will clean up the front end asap

--- app/controllers/burgers_controller.js
+++ app/controllers/burgers_controller.js
@@ -1,54 +1,16 @@
-// Importing Dependencies
-// -------------------------------------------------
 var express = require('express');
 var router = express.Router();
-var models = require('../models');
-var Burger = models.Burger;
-
-
-// API routers
 
 router.get('/', (req,res) => {
-    Burger.findAll().then((data) => {
-        res.json(data)
-    })
+    res.render('index')
 })
 
-router.get('/all', (req,res) => {
-    Burger.findAll().then((data) => {
-        res.json(data)
-    })
+router.get('/show/:id', (req,res) => {
+    res.render('show')
 })
 
-router.get('/:id', (req,res) => {
-    Burger.findById(req.params.id).then((data) => {
-        res.json(data)
-    })
+router.get('/edit/:id', (req,res) => {
+    res.render('edit')
 })
 
-router.post('/new', (req,res) => {
-    Burger.create(req.body)
-    .then((data) => {
-        res.json(data);
-    })
-})
-
-router.put('/:id',(req,res) => {
-    console.log(req.body)
-    Burger.update(req.body,{where: {id: req.params.id}})
-    .then((data) => {
-        res.json(data)
-    })
-})
-
-router.delete('/:id', (req,res) => {
-    Burger.destroy({where: {id: req.params.id}})
-    .then((data) => {
-        res.json(data)
-    })
-})
-
-
-// Exporting Router to server.js
-// -------------------------------------------------
 module.exports = router;
